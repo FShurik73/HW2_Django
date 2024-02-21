26ea0d1006ad5fa9dd1e8534005388b73df15655
FShurik73 <115178325+FShurik73@users.noreply.github.com>
2024-02-21 16:22:33 +0300

    Update views.py

--- Homework2/store_app/views.py
+++ Homework2/store_app/views.py
@@ -22,9 +22,8 @@ def sorted_basket(request, client_id, days_ago):
     products = []
     product_set = []
     now = datetime.now()
-    before = now - timedelta(days=days_ago)
     client = Client.objects.filter(pk=client_id).first()
-    orders = Order.objects.filter(client=client, order_date__range=(before, now)).all()
+    orders = Order.objects.filter(client=client, order_date__gte=now - timedelta(days=days_ago))
     for order in orders:
         products = order.product.all()
         for product in products:
